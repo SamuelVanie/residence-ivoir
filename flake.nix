8f851907cd59c97050c073665d579378dde06df6
SamuelVanie <damsektry@gmail.com>
2025-03-02 19:42:01 -0500

    ci: write the reproductible environment using nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Django environment for residence-ivoir project";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python313
+            python313Packages.django
+          ];
+
+          shellHook = ''
+             echo "Thanks y'all";
+          '';
+        };
+      });
+}
