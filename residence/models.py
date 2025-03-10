f0a78353bc80e764ab00bc234ce9a38185bc45ce
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-03-10 14:53:17 -0400

    fix: issues on the Residence model.
    Got back the fields that matches the specifications

--- residence/models.py
+++ residence/models.py
@@ -3,10 +3,14 @@ from django.conf import settings
 
 class Residence(models.Model):
     # Fields for residence characteristics (as described in roadmap)
+    name = models.CharField(max_length=255)
     number_of_rooms = models.IntegerField()
+    promotional_price = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    location = models.CharField(max_length=255)
+    is_available = models.BooleanField(default=True, help_text="Residence available for getting booked rn?")
+    location = models.CharField(max_length=255, default='Yamoussoukro')
     description = models.TextField()
+    image = models.ImageField(upload_to='residence_images/', blank=True)
 
     def __str__(self):
         return f"Residence at {self.location} - {self.number_of_rooms} rooms"
