29c54c441906836ee3206a91b2858c7496f6f265
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-03-12 02:07:49 +0100

    fix: image rendering of the residence on the listing page

--- residenceivoir/urls.py
+++ residenceivoir/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from residence import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('residences/', views.residence_list, name='residence_list'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
