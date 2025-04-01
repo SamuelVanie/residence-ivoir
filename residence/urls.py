1e202bbaa0586fb4cbec2d7b32b7ca34f4d16068
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-04-01 22:57:49 +0200

    feat: add the reserve_link builder url to views

--- residence/urls.py
+++ residence/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('filter_by_rooms/rooms<int:rooms>/', views.filter_residences_by_rooms, name='filter_residences'),
     path('filter_by_budget/', views.filter_residences_by_budget, name='filter_residences_by_budget'),
     path('contact/whatsapp/', views.whatsapp_contact_link, name='whatsapp_contact_link'),
+    path('reserve/whatsapp/', views.whatsapp_reserve_link, name='whatsapp_reserve_link')
 ]
